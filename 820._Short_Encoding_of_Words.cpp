df61a8416f4e399b300c7df51044641d8c352f94
maverick <adity09@outlook.com>
2021-08-10 22:06:28 +0530

    new format

---
+++ 820._Short_Encoding_of_Words.cpp
@@ -0,0 +1,78 @@
+#include<bits/stdc++.h>
+using namespace std;
+class Solution {
+public:
+    class node {
+    public:
+        bool word;
+        vector<node*>nodes;
+        node() {
+            word = false;
+            nodes.resize(26, nullptr);
+        }
+    };
+    class trie {
+    public:
+        node* root;
+        trie() {
+            root = new node();
+        }
+        int search(node* it) {
+            if (!it)
+                return 0;
+            int tmp = 0;
+            for (node* nd : it->nodes)
+                tmp = max(tmp, search(nd));
+            return 1 + tmp;
+        }
+        int insert(string& s) {
+            reverse(s.begin(), s.end());
+            node* cur = root;
+            int cnt = 0;
+            node* nd = nullptr;
+            for (const char& c : s) {
+                if (cur->nodes[c - 'a'] == nullptr)
+                    cur->nodes[c - 'a'] = new node();
+                else {
+                    cnt++;
+                    nd = cur->nodes[c - 'a'];
+                }
+                cur = cur->nodes[c - 'a'];
+            }
+            if (cur->word)
+                return 0;
+            cur->word = true;
+            for (node* it : cur->nodes)
+                if (it)
+                    return 0;
+            if (nd) {
+                int tmp = 0;
+                for (node* it : nd->nodes) {
+                    if (it) {
+                        if (!tmp)
+                            ++tmp;
+                        else {
+                            cnt = 0;
+                            break;
+                        }
+                    }
+                }
+            }
+            return s.length() - cnt - (cnt != 0);
+        }
+    };
+
+    int minimumLengthEncoding(vector<string>& v) {
+        int res = 0;
+        trie tr;
+        for (auto& it : v) {
+            int tmp = tr.insert(it);
+            res += tmp + 1;
+            cout << tmp << ' ';
+        }
+        return res;
+    }
+};
+int main() {
+    return 0;
+}
--- README.md
+++ README.md
@@ -1,13 +1,29 @@
-# leetcode-solutions
-Solutions to the problems I solve  at leetcode.
+<h2>542. 01 Matrix</h2><h3>Medium</h3><hr><div><p>Given an <code>m x n</code> binary matrix <code>mat</code>, return <em>the distance of the nearest </em><code>0</code><em> for each cell</em>.</p>
 
-Regularly updated with solutions to more questions.
+<p>The distance between two adjacent cells is <code>1</code>.</p>
 
-Languages used: C++
+<p>&nbsp;</p>
+<p><strong>Example 1:</strong></p>
+<img alt="" src="https://assets.leetcode.com/uploads/2021/04/24/01-1-grid.jpg" style="width: 253px; height: 253px;">
+<pre><strong>Input:</strong> mat = [[0,0,0],[0,1,0],[0,0,0]]
+<strong>Output:</strong> [[0,0,0],[0,1,0],[0,0,0]]
+</pre>
 
-For any query, drop it @ adity09@outlook.com
+<p><strong>Example 2:</strong></p>
+<img alt="" src="https://assets.leetcode.com/uploads/2021/04/24/01-2-grid.jpg" style="width: 253px; height: 253px;">
+<pre><strong>Input:</strong> mat = [[0,0,0],[0,1,0],[1,1,1]]
+<strong>Output:</strong> [[0,0,0],[0,1,0],[1,2,1]]
+</pre>
 
-I'm at :-
-          linkedin.com/adityasaxena09
+<p>&nbsp;</p>
+<p><strong>Constraints:</strong></p>
 
-Good luck!
+<ul>
+	<li><code>m == mat.length</code></li>
+	<li><code>n == mat[i].length</code></li>
+	<li><code>1 &lt;= m, n &lt;= 10<sup>4</sup></code></li>
+	<li><code>1 &lt;= m * n &lt;= 10<sup>4</sup></code></li>
+	<li><code>mat[i][j]</code> is either <code>0</code> or <code>1</code>.</li>
+	<li>There is at least one <code>0</code> in <code>mat</code>.</li>
+</ul>
+</div>
