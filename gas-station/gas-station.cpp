d07f7a6e12ef2ee64fef4fc5aed112ab6ca844f5
Aditya Saxena <adity09@outlook.com>
2021-09-08 13:29:34 +0530

    Update gas-station.cpp

--- gas-station/gas-station.cpp
+++ gas-station/gas-station.cpp
@@ -1,33 +1,22 @@
 class Solution {
 public:
-  class cmp{
-  public:
-    bool operator()(const pair<int,int>&p,const pair<int,int>&q){
-      return p.first<q.first;
-    }
-  };
-    int canCompleteCircuit(vector<int>& gas, vector<int>& costs) {
-        priority_queue<pair<int,int>,vector<pair<int,int>>,cmp>pq;
-        int sz=gas.size();
-        for(int i=0;i<sz;++i)
-          pq.push({gas[i]-costs[i],i});
-        while(!pq.empty() and pq.top().first>-1){
-          int i,in=pq.top().second,t=0,pre=0;
-          //cout<<in<<' ';
-          pq.pop();
-          for(i=0;i<=sz;++i){
-            int j=(i+in)%sz;
-            //cout<<j<<' '<<t<<' '<<pre<<'\n';
-            t-=pre;
-            pre=costs[j];
-            if(t<0)
-              break;
-            t+=gas[j];
-          }
-          if(i==sz+1 and t>-1)
-            return in;
-          //cout<<"failed for  "<<in<<'\n';
+    int canCompleteCircuit(vector<int>& gas, vector<int>& cost) {
+        int res = -1, sz = gas.size();
+        for (int i = 0;i < sz;) {
+            int fuel = gas[i], j = 1;
+            for (;j <= sz;++j) {
+                int cur = (i + j) % sz, pre = cur ? cur - 1 : sz - 1;
+                fuel -= cost[pre];
+                if (fuel < 0)
+                    break;
+                fuel += gas[cur];
+            }
+            if (j == sz + 1) {
+                res = i;
+                break;
+            }
+            i +=j;
         }
-        return -1;
+        return res;
     }
 };
