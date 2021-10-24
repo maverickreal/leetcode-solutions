9c06d61a5d9dfe51cd7efd0875db38f3f5b65a0e
Aditya Saxena <adity09@outlook.com>
2021-10-24 12:54:18 +0530

    Time: 97 ms (5.45%), Space: 107.8 MB (5.13%) - LeetHub

---
+++ number-of-enclaves/number-of-enclaves.java
@@ -0,0 +1,67 @@
+    class Solution {
+
+        public Solution() {
+        }
+
+            static private class group {
+            public Integer f;
+            public Integer s;
+        
+            public group(Integer f, Integer s) {
+            this.f = f;
+            this.s = s;
+            }
+            }
+
+        static private Integer m, n;
+
+        static private group func(ArrayList<ArrayList<Integer>> g, Integer r, Integer c) {
+            if (r <0 || c <0 || r >m-1 || c>n-1 || g.get(r).get(c)==0)
+                return new group(0,1);
+            group ans=new group(1,1);
+            if (r == m - 1 || r == 0 || c == n - 1 || c == 0)
+                ans.s=0;
+            g.get(r).set(c, 0);
+            group res = func(g, r + 1, c);
+            if (res.s == 0)
+            ans.s=0;
+            ans.f+=res.f;
+            res = func(g, r - 1, c);
+            if (res.s == 0)
+            ans.s=0;
+            ans.f+=res.f;
+            res = func(g, r, c + 1);
+            if (res.s == 0)
+            ans.s=0;
+            ans.f+=res.f;
+            res = func(g, r, c - 1);
+            if (res.s == 0)
+            ans.s=0;
+            ans.f+=res.f;
+            //System.out.println(r.toString()+"  "+c.toString()+"  "+res.f.toString()+"  "+res.s.toString());
+            return ans;
+        }
+
+        public int numEnclaves(int[][] g) {
+            ArrayList<ArrayList<Integer>> ar = new ArrayList<>();
+            for (int[] x : g) {
+                ArrayList<Integer> tmp = new ArrayList<>();
+                for (Integer y : x)
+                    tmp.add(y);
+                ar.add(tmp);
+            }
+            m = ar.size();
+            n = ar.get(0).size();
+            Integer res = 0;
+            for (Integer i = 0; i < m; ++i) {
+                for (Integer j = 0; j < n; ++j) {
+                    group tmp = func(ar, i, j);
+                    if (tmp.s==1){
+                        //System.out.println(i.toString() + " " + j.toString()+"  "+tmp);
+                        res += tmp.f;
+                    }
+                }
+            }
+            return res;
+        }
+    }
