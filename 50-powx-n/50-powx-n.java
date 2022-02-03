b514cf6b9607334acccbbcf185b45ae9e47bc688
Aditya Saxena <adity09@outlook.com>
2022-02-03 23:10:04 +0530

    Time: 11 ms (5.57%), Space: 45.1 MB (5.32%) - LeetHub

---
+++ 50-powx-n/50-powx-n.java
@@ -0,0 +1,31 @@
+class Solution {
+    static private double func(double x, long n, double res){
+        if(n==0)
+            return res;
+        //System.out.println(res+"    "+n);
+        if ((n & 1) == 1){
+            res *= x;
+            --n;
+        }
+        else{
+        x*=x;
+        n >>= 1;
+        }
+        return func(x, n, res);
+    }
+    
+        public double myPow(double x, long n) {
+            if(n==0)
+                return 1;
+            if(n==1 || x==0 || x==1)
+                return x;
+            boolean fl=n<0;
+            if(fl)
+                n=-n;
+            System.out.println(n+" ----");
+            double res=func(x, n, 1);
+            if(fl)
+                res=1/res;
+            return res;
+    }
+}
