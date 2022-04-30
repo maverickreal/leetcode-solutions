5f163b1bdd384c41314d49059e0cafd9aceebed8
Aditya Saxena <adity09@outlook.com>
2022-04-30 18:45:22 +0530

    Added solution - LeetHub

---
+++ Min distance between two given nodes of a Binary Tree - GFG/min-distance-between-two-given-nodes-of-a-binary-tree.cpp
@@ -0,0 +1,153 @@
+// { Driver Code Starts
+//Initial Template for C++
+
+
+#include <bits/stdc++.h>
+using namespace std;
+#define MAX_HEIGHT 100000
+
+// Tree Node
+struct Node {
+    int data;
+    Node* left;
+    Node* right;
+};
+
+// Utility function to create a new Tree Node
+Node* newNode(int val) {
+    Node* temp = new Node;
+    temp->data = val;
+    temp->left = NULL;
+    temp->right = NULL;
+
+    return temp;
+}
+
+
+// Function to Build Tree
+Node* buildTree(string str) {
+    // Corner Case
+    if (str.length() == 0 || str[0] == 'N') return NULL;
+
+    // Creating vector of strings from input
+    // string after spliting by space
+    vector<string> ip;
+
+    istringstream iss(str);
+    for (string str; iss >> str;) ip.push_back(str);
+
+    // Create the root of the tree
+    Node* root = newNode(stoi(ip[0]));
+
+    // Push the root to the queue
+    queue<Node*> queue;
+    queue.push(root);
+
+    // Starting from the second element
+    int i = 1;
+    while (!queue.empty() && i < ip.size()) {
+
+        // Get and remove the front of the queue
+        Node* currNode = queue.front();
+        queue.pop();
+
+        // Get the current node's value from the string
+        string currVal = ip[i];
+
+        // If the left child is not null
+        if (currVal != "N") {
+
+            // Create the left child for the current node
+            currNode->left = newNode(stoi(currVal));
+
+            // Push it to the queue
+            queue.push(currNode->left);
+        }
+
+        // For the right child
+        i++;
+        if (i >= ip.size()) break;
+        currVal = ip[i];
+
+        // If the right child is not null
+        if (currVal != "N") {
+
+            // Create the right child for the current node
+            currNode->right = newNode(stoi(currVal));
+
+            // Push it to the queue
+            queue.push(currNode->right);
+        }
+        i++;
+    }
+
+    return root;
+}
+
+
+ // } Driver Code Ends
+/* A binary tree node
+struct Node
+{
+    int data;
+    Node* left, * right;
+}; */
+
+class Solution{
+    public:
+    /* Should return minimum distance between a and b
+    in a tree with given root*/
+typedef long long ll;
+#define endl '\n'
+const ll mod = 1e9 + 7;
+
+bool getPath(Node* root, ll nd, vector<Node*>& v) {
+    if (!root)
+        return false;
+    v.push_back(root);
+    if (root->data == nd)
+        return true;
+    bool fl = getPath(root->left, nd, v);
+    if (!fl)
+        fl = getPath(root->right, nd, v);
+    if (!fl)
+        v.pop_back();
+    return fl;
+}
+
+int findDist(Node* root, int a, int b) {
+    vector<Node*>x, y;
+    getPath(root, a, x);
+    getPath(root, b, y);
+
+    ll i = -1;
+    while (++i < min(x.size(), y.size()) && x[i] == y[i]);
+    return x.size() - i + y.size() - i;
+}
+};
+
+// { Driver Code Starts.
+
+/* Driver program to test size function*/
+
+  
+
+int main() {
+
+   
+    int t;
+    scanf("%d ", &t);
+    while (t--) {
+        string s, ch;
+        getline(cin, s);
+        
+        Node* root = buildTree(s);
+        int n1, n2;
+        scanf("%d %d ", &n1, &n2);
+        Solution ob;
+        cout << ob.findDist(root, n1, n2) << endl;
+     
+    }
+    return 0;
+}
+  // } Driver Code Ends
