5cebf8e889ca6a6805ab4bb214b47028db4104f7
Aditya Saxena <adity09@outlook.com>
2022-06-25 22:30:33 +0530

    Time: 137 ms (50.00%), Space: 48 MB (100.00%) - LeetHub

---
+++ 2317-maximum-xor-after-operations/2317-maximum-xor-after-operations.cpp
@@ -0,0 +1,28 @@
+class Solution {
+    typedef int ll;
+    typedef long double ld;
+    typedef pair<ll, ll> pi;
+#define vi(x) vector<x>
+#define endl '\n'
+    const ll mod = 1e9;
+public:
+    int maximumXOR(vector<int>& v) {
+        ll res = 0, sz = v.size(), cnt = 0;
+        while (true) {
+            bool fl = false, zero = true;
+            for (ll& it : v) {
+                if (it & 1)
+                    fl = true;
+                it >>= 1;
+                if (it)
+                    zero = false;
+            }
+            if (fl)
+                res |= (1 << cnt);
+            ++cnt;
+            if (zero)
+                break;
+        }
+        return res;
+    }
+};
