ebc8fa99e7fe9ab997eeeae82208a67d3a3c6884
Aditya Saxena <adity09@outlook.com>
2022-07-20 21:00:23 +0530

    Time: 89 ms (27.29%), Space: 14.4 MB (88.34%) - LeetHub

--- 160-intersection-of-two-linked-lists/160-intersection-of-two-linked-lists.cpp
+++ 160-intersection-of-two-linked-lists/160-intersection-of-two-linked-lists.cpp
@@ -1,28 +1,41 @@
-/**
- * Definition for singly-linked list.
- * struct ListNode {
- *     int val;
- *     ListNode *next;
- *     ListNode(int x) : val(x), next(NULL) {}
- * };
- */
 class Solution {
+    typedef long long ll;
+    typedef pair<ll, ll> pi;
+#define vi(x) vector<x>
+#define pb push_back
+    const ll mod = 1e9 + 7;
+    const char nl = '\n';
 public:
-typedef long long ll;
-#define ln ListNode
-
-ListNode* getIntersectionNode(ListNode* x, ListNode* y) {
-    ln* a = x, * b = y;
-    while (a || b) {
-        if (!a)
-            a = x;
-        if (!b)
-            b = y;
-        if (a == b)
-            return a;
-        a = a->next;
-        b = b->next;
+    ListNode* getIntersectionNode(ListNode* headA, ListNode* headB) {
+        ll sza = 0, szb = 0;
+        auto ita = headA, itb = headB;
+        while (ita) {
+            ++sza;
+            ita = ita->next;
+        }
+        while (itb) {
+            ++szb;
+            itb = itb->next;
+        }
+        if (szb > sza) {
+            sza += szb;
+            szb = sza - szb;
+            sza -= szb;
+            ita = headB;
+            itb = headA;
+        }
+        else {
+            ita = headA;
+            itb = headB;
+        }
+        ll dif = sza - szb;
+        while (dif--) {
+            ita = ita->next;
+        }
+        while (ita && itb && ita != itb) {
+            ita = ita->next;
+            itb = itb->next;
+        }
+        return ita;
     }
-    return nullptr;
-}
 };
