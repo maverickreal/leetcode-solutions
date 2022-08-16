fc7492fd9b163996e23571716a689afed33d57a9
Aditya Saxena <adity09@outlook.com>
2022-08-16 13:27:02 +0530

    Time: 3 ms (60.19%), Space: 8.4 MB (5.42%) - LeetHub

---
+++ 377-combination-sum-iv/377-combination-sum-iv.cpp
@@ -0,0 +1,31 @@
+class Solution {
+    typedef long long ll;
+    typedef pair<ll, ll> pi;
+#define vi(x) vector<x>
+#define pb push_back
+    const ll mod = 1e9 + 7;
+    const char nl = '\n';
+public:
+    ll sz;
+    vi(vi(ll))dp;
+    ll func(const vi(int)& v, ll in, ll n) {
+        if (n == 0) {
+            return 1;
+        }
+        if (n < 0 || in == sz) {
+            return 0;
+        }
+        if (dp[in][n] == -1) {
+            dp[in][n] = func(v, in + 1, n);
+            if (v[in] <= n) {
+                dp[in][n] += func(v, 0, n - v[in]);
+            }
+        }
+        return dp[in][n];
+    }
+    int combinationSum4(const vi(int)& v, ll tar) {
+        sz = v.size();
+        dp.resize(sz, vi(ll)(tar + 1, -1));
+        return func(v, 0, tar);
+    }
+};
