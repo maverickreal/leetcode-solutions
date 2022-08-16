64d305b31ce6762c95a01a16523ffbaa4c9ea1ff
Aditya Saxena <adity09@outlook.com>
2022-08-16 12:10:39 +0530

    Time: 103 ms (5.05%), Space: 19.3 MB (11.47%) - LeetHub

---
+++ 1143-longest-common-subsequence/1143-longest-common-subsequence.cpp
@@ -0,0 +1,28 @@
+class Solution {
+    typedef long long ll;
+    typedef pair<ll, ll> pi;
+#define vi(x) vector<x>
+#define pb push_back
+    const ll mod = 1e9 + 7;
+    const char nl = '\n';
+public:
+    ll szs, szt;
+    vi(vi(ll))dp;
+    ll func(const string& s, const string& t, ll is, ll it) {
+        if (is >= szs || it >= szt) {
+            return 0;
+        }
+        if (dp[is][it] == -1) {
+            dp[is][it] = max(func(s, t, is + 1, it), func(s, t, is, it + 1));
+            if (s[is] == t[it]) {
+                dp[is][it] = max(dp[is][it], func(s, t, is + 1, it + 1) + 1);
+            }
+        }
+        return dp[is][it];
+    }
+    ll longestCommonSubsequence(const string& s, const string& t) {
+        szs = s.length(), szt = t.length();
+        dp.resize(szs, vi(ll)(szt, -1));
+        return func(s, t, 0, 0);
+    }
+};
