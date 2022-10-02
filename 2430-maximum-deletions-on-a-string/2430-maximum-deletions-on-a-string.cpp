05c3feb71d6f37e6e04812464edf3db93f5e10e2
Aditya Saxena <adity09@outlook.com>
2022-10-02 12:18:48 +0530

    Time: 1945 ms (12.50%), Space: 447.1 MB (12.50%) - LeetHub

---
+++ 2430-maximum-deletions-on-a-string/2430-maximum-deletions-on-a-string.cpp
@@ -0,0 +1,45 @@
+class Solution{
+typedef long long ll;
+typedef pair<ll, ll> pi;
+#define vi(x) vector<x>
+#define pb push_back
+const ll mod = 1e9 + 7;
+const char nl = '\n';
+public:
+    ll n;
+    vi(ll)dp;
+    vi(vi(ll))pref;
+    ll func(const string&s, ll in){
+        if(in>=n){
+            return 0;
+        }
+        ll&ans=dp[in];
+        if(ans==-1){
+            ans=1;
+            for(ll j=in;;++j){
+                ll k=j-in+1;
+                k=j+1+k;
+                --k;
+                if(k>=n){
+                    break;
+                }
+                if(pref[in][j]==pref[j+1][k]){
+                    ans=max(ans, 1+func(s, j+1));
+                }
+            }
+        }
+        return ans;
+    }
+    int deleteString(const string&s) {
+        n=s.size();
+        dp.assign(n, -1), pref.assign(n, vi(ll)(n, -1));
+        for(ll i=0;i<n;++i){
+            pref[i][i]=s[i]-'a'+1;
+            for(ll j=i+1, pPow=31;j<n;++j){
+                pref[i][j] = (pref[i][j-1] + (s[j] - 'a' + 1) * pPow) % mod;
+                pPow = (pPow * 31) % mod;
+            }
+        }
+        return func(s, 0);
+    }
+}; // j+1 +j-i
