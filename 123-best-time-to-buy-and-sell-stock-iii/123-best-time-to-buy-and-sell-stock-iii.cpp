07cf58e8e6c84f458fb7833deea8c700737022e8
Aditya Saxena <adity09@outlook.com>
2022-10-08 21:58:58 +0530

    Time: 898 ms (35.52%), Space: 282.3 MB (5.01%) - LeetHub

---
+++ 123-best-time-to-buy-and-sell-stock-iii/123-best-time-to-buy-and-sell-stock-iii.cpp
@@ -0,0 +1,31 @@
+class Solution {
+typedef long long ll;
+typedef pair<ll, ll> pi;
+#define vi(x) vector<x>
+#define pb push_back
+const ll mod = 1e9 + 7;
+const char nl = '\n';
+public:
+    vi(vi(vi(ll)))dp;
+    ll n;
+    ll func(const vi(int)&v, ll in, bool bought, ll rem){
+        if(in>=n || rem<1){
+            return 0;
+        }
+        ll&ans=dp[in][rem][bought];
+        if(ans==-1){
+            if(bought){
+                ans=max(func(v, in+1, bought, rem), v[in]+func(v, in+1, 0, rem-1));
+            }
+            else{
+                ans=max(func(v, in+1, 0, rem), -v[in]+func(v, in+1, 1, rem));
+            }
+        }
+        return ans;
+    }
+    int maxProfit(vector<int>&v, ll k=2) {
+        n=v.size();
+        dp.assign(n, vi(vi(ll))(k+1, vi(ll)(2, -1)));
+        return func(v, 0, 0, k);
+    }
+};
