3f9ebe6323e58f4727765910b377194270c307ac
Aditya Saxena <adity09@outlook.com>
2022-10-08 21:56:34 +0530

    Time: 175 ms (13.37%), Space: 28.5 MB (5.21%) - LeetHub

--- 188-best-time-to-buy-and-sell-stock-iv/188-best-time-to-buy-and-sell-stock-iv.cpp
+++ 188-best-time-to-buy-and-sell-stock-iv/188-best-time-to-buy-and-sell-stock-iv.cpp
@@ -6,26 +6,26 @@ typedef pair<ll, ll> pi;
 const ll mod = 1e9 + 7;
 const char nl = '\n';
 public:
+    vi(vi(vi(ll)))dp;
     ll n;
-    vi(vi(ll))dp;
-    ll func(const vi(int)&v, ll in, ll rem){
-        if(rem<1 || in>=n){
+    ll func(const vi(int)&v, ll in, bool bought, ll rem){
+        if(in>=n || rem<1){
             return 0;
         }
-        ll&ans=dp[in][rem];
+        ll&ans=dp[in][rem][bought];
         if(ans==-1){
-            ans=func(v, in+1, rem);
-            for(ll i=in+1;i<n;++i){
-                if(v[i]>v[in]){
-                    ans=max(ans, v[i]-v[in]+func(v, i+1, rem-1));
-                }
+            if(bought){
+                ans=max(func(v, in+1, bought, rem), v[in]+func(v, in+1, 0, rem-1));
+            }
+            else{
+                ans=max(func(v, in+1, 0, rem), -v[in]+func(v, in+1, 1, rem));
             }
         }
         return ans;
     }
     int maxProfit(int k, vector<int>&v) {
         n=v.size();
-        dp.assign(n, vi(ll)(k+1, -1));
-        return func(v, 0, k);
+        dp.assign(n, vi(vi(ll))(k+1, vi(ll)(2, -1)));
+        return func(v, 0, 0, k);
     }
 };
