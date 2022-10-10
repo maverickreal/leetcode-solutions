3af1d8fb5f6070e5173045d3dd983f82f7e4369a
Aditya Saxena <adity09@outlook.com>
2022-10-10 21:26:18 +0530

    Time: 11 ms (94.98%), Space: 18.9 MB (20.08%) - LeetHub

--- 1269-number-of-ways-to-stay-in-the-same-place-after-some-steps/1269-number-of-ways-to-stay-in-the-same-place-after-some-steps.cpp
+++ 1269-number-of-ways-to-stay-in-the-same-place-after-some-steps/1269-number-of-ways-to-stay-in-the-same-place-after-some-steps.cpp
@@ -6,16 +6,13 @@ typedef pair<ll, ll> pi;
 const ll mod = 1e9 + 7;
 const char nl = '\n';
 public:
-    unordered_map<ll,unordered_map<ll,ll>>dp;
+    vi(vi(ll))dp;
     ll func(ll in, const int&n, ll rem){
         if(in<0 || in>=n || rem<in ){
             return 0;
         }
         if(rem==in){
             return 1;
-        }
-        if(dp[in].find(rem)==dp[in].end()){
-            dp[in][rem]=-1;
         }
         ll&ans=dp[in][rem];
         if(ans==-1){
@@ -24,7 +21,7 @@ public:
         return ans;
     }
     int numWays(int k, int n) {
-        //dp.assign(n, vi(ll)(k+1, -1));
+        dp.assign(min(n, k), vi(ll)(k+1, -1));
         return func(0, n, k);
     }
 };
