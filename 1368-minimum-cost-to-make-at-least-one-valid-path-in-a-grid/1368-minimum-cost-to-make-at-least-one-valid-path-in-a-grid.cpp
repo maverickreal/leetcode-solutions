3e35785c54e0ed6f54593f3abd1c2929d7b3d511
Aditya Saxena <adity09@outlook.com>
2022-10-17 11:37:47 +0530

    Time: 472 ms (12.28%), Space: 30 MB (18.13%) - LeetHub

---
+++ 1368-minimum-cost-to-make-at-least-one-valid-path-in-a-grid/1368-minimum-cost-to-make-at-least-one-valid-path-in-a-grid.cpp
@@ -0,0 +1,59 @@
+class Solution {
+typedef long long ll;
+typedef pair<ll, ll> pi;
+#define vi(x) vector<x>
+#define pb push_back
+const ll mod = 1e9 + 7;
+const char nl = '\n';
+public:
+    class cmp{
+    public:
+        bool operator()(const vi(ll)&a, const vi(ll)&b){
+            return (a[2]>b[2]);
+        }
+    };
+    int minCost(vector<vector<int>>&v) {
+        ll m=v.size(), n=v[0].size(), ans=0;
+        vi(vi(ll))cost(m, vi(ll)(n, LLONG_MAX));
+        priority_queue<vi(ll), vi(vi(ll)), cmp>pq;
+        pq.push({m-1, n-1, 0});
+        cost[m-1][n-1]=0;
+        while(!pq.empty()){
+            auto nd=pq.top();
+            pq.pop();
+            if(nd[0]==0 && nd[1]==0){
+                ans=nd[2];
+                break;
+            }
+            if(nd[0]>0){
+                ll res=nd[2]+(v[nd[0]-1][nd[1]]!=3);
+                if(res<cost[nd[0]-1][nd[1]]){
+                    cost[nd[0]-1][nd[1]]=res;
+                    pq.push({nd[0]-1, nd[1], res});
+                }
+            }
+            if(nd[1]>0){
+                ll res=nd[2]+(v[nd[0]][nd[1]-1]!=1);
+                if(res<cost[nd[0]][nd[1]-1]){
+                    cost[nd[0]][nd[1]-1]=res;
+                    pq.push({nd[0], nd[1]-1, res});
+                }
+            }
+            if(nd[0]<m-1){
+                ll res=nd[2]+(v[nd[0]+1][nd[1]]!=4);
+                if(res<cost[nd[0]+1][nd[1]]){
+                    cost[nd[0]+1][nd[1]]=res;
+                    pq.push({nd[0]+1, nd[1], res});
+                }
+            }
+            if(nd[1]<n-1){
+                ll res=nd[2]+(v[nd[0]][nd[1]+1]!=2);
+                if(res<cost[nd[0]][nd[1]+1]){
+                    cost[nd[0]][nd[1]+1]=res;
+                    pq.push({nd[0], nd[1]+1, res});
+                }
+            }
+        }
+        return ans;
+    }
+};
