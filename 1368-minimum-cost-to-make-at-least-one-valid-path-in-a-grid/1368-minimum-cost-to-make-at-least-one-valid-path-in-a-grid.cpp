20faf2af166f5098fbd193b374f48b834d488cc7
Aditya Saxena <adity09@outlook.com>
2022-10-17 11:45:59 +0530

    Time: 1280 ms (5.07%), Space: 162.1 MB (5.07%) - LeetHub

--- 1368-minimum-cost-to-make-at-least-one-valid-path-in-a-grid/1368-minimum-cost-to-make-at-least-one-valid-path-in-a-grid.cpp
+++ 1368-minimum-cost-to-make-at-least-one-valid-path-in-a-grid/1368-minimum-cost-to-make-at-least-one-valid-path-in-a-grid.cpp
@@ -6,24 +6,18 @@ typedef pair<ll, ll> pi;
 const ll mod = 1e9 + 7;
 const char nl = '\n';
 public:
-    class cmp{
-    public:
-        bool operator()(const vi(ll)&a, const vi(ll)&b){
-            return (a[2]>b[2]);
-        }
-    };
     int minCost(vector<vector<int>>&v) {
         ll m=v.size(), n=v[0].size();
         vi(vi(ll))cost(m, vi(ll)(n, LLONG_MAX));
-        priority_queue<vi(ll), vi(vi(ll)), cmp>pq;
-        pq.push({m-1, n-1, 0});
+        list<vi(ll)>l;
+        l.pb({m-1, n-1, 0});
         cost[m-1][n-1]=0;
-        while(!pq.empty()){
-            auto nd=pq.top();
-            pq.pop();
-            if(nd[0]==0 && nd[1]==0){
-                break;
-            }
+        while(!l.empty()){
+            auto nd=l.front();
+            l.pop_front();
+            // if(nd[0]==0 && nd[1]==0){
+            //     break;
+            // }
             if(cost[nd[0]][nd[1]]<nd[2]){
                 continue;
             }
@@ -31,28 +25,28 @@ public:
                 ll res=nd[2]+(v[nd[0]-1][nd[1]]!=3);
                 if(res<cost[nd[0]-1][nd[1]]){
                     cost[nd[0]-1][nd[1]]=res;
-                    pq.push({nd[0]-1, nd[1], res});
+                    l.pb({nd[0]-1, nd[1], res});
                 }
             }
             if(nd[1]>0){
                 ll res=nd[2]+(v[nd[0]][nd[1]-1]!=1);
                 if(res<cost[nd[0]][nd[1]-1]){
                     cost[nd[0]][nd[1]-1]=res;
-                    pq.push({nd[0], nd[1]-1, res});
+                    l.pb({nd[0], nd[1]-1, res});
                 }
             }
             if(nd[0]<m-1){
                 ll res=nd[2]+(v[nd[0]+1][nd[1]]!=4);
                 if(res<cost[nd[0]+1][nd[1]]){
                     cost[nd[0]+1][nd[1]]=res;
-                    pq.push({nd[0]+1, nd[1], res});
+                    l.pb({nd[0]+1, nd[1], res});
                 }
             }
             if(nd[1]<n-1){
                 ll res=nd[2]+(v[nd[0]][nd[1]+1]!=2);
                 if(res<cost[nd[0]][nd[1]+1]){
                     cost[nd[0]][nd[1]+1]=res;
-                    pq.push({nd[0], nd[1]+1, res});
+                    l.pb({nd[0], nd[1]+1, res});
                 }
             }
         }
