a0dff85dd6d97c61b3d891537505d9216e679b75
Aditya Saxena <adity09@outlook.com>
2022-10-18 13:08:38 +0530

    Time: 232 ms (58.24%), Space: 58.4 MB (65.41%) - LeetHub

--- 2392-build-a-matrix-with-conditions/2392-build-a-matrix-with-conditions.cpp
+++ 2392-build-a-matrix-with-conditions/2392-build-a-matrix-with-conditions.cpp
@@ -1,65 +1,59 @@
 class Solution {
-    typedef long long ll;
-    typedef pair<ll, ll> pi;
+typedef long long ll;
+typedef pair<ll, ll> pi;
 #define vi(x) vector<x>
 #define pb push_back
-    const ll mod = 1e9 + 7;
-    const char nl = '\n';
+const ll mod = 1e9 + 7;
+const char nl = '\n';
 public:
-    void topo(const vi(unordered_set<ll>)& g, vi(ll)& res) {
-        ll k = g.size(), vis = 0;
-        vi(ll)indeg(k, 0);
-        list<ll>l;
-        for (ll i = 0;i < k;++i) {
-            for (ll child : g[i]) {
-                ++indeg[child];
-            }
+    void topo(const vi(vi(int))&e, const ll&n, vi(ll)&v){
+        vi(vi(ll))g(n+1);
+        vi(ll)ind(1+n);
+        for(const auto&edge:e){
+            ++ind[edge[1]];
+            g[edge[0]].pb(edge[1]);
         }
-        for (ll i = 0;i < k;++i) {
-            if (indeg[i] == 0) {
+        list<ll>l;
+        for(ll i=1;i<=n;++i){
+            if(!ind[i]){
                 l.pb(i);
             }
         }
-        while (!l.empty()) {
-            ll it = l.front();
+        while(!l.empty()){
+            ll nd=l.front();
             l.pop_front();
-            res.pb(it);
-            for (ll child : g[it]) {
-                --indeg[child];
-                if (indeg[child] == 0) {
+            v.pb(nd);
+            for(ll child:g[nd]){
+                if(--ind[child]==0){
                     l.pb(child);
                 }
             }
-            ++vis;
         }
-        if (vis != k) {
-            res.clear();
+        if(v.size()!=n){
+            v.clear();
         }
     }
-    vector<vector<int>> buildMatrix(int k, vector<vector<int>>& rc, vector<vector<int>>& cc) {
-        vi(unordered_set<ll>)gr(k), gc(k);
-        for (auto it : rc) {
-            gr[it[0] - 1].insert(it[1] - 1);
+    vector<vector<int>> buildMatrix(int k, vector<vector<int>>&rc, vector<vector<int>>&cc) {
+        vi(ll)r, c;
+        vi(vi(int))ans;
+        topo(rc, k, r);
+        if(r.empty()){
+            return ans;
         }
-        for (auto it : cc) {
-            gc[it[0] - 1].insert(it[1] - 1);
+        topo(cc, k, c);
+        if(c.empty()){
+            return ans;
         }
-        vi(ll)ro, co;
-        topo(gr, ro);
-        if (ro.size() < k) {
-            return {};
-        }
-        topo(gc, co);
-        if (co.size() < k) {
-            return {};
-        }
-        vi(vi(int))ans(k, vi(int)(k, 0));
-        vi(ll)ump(k, -1);
-        for (ll i = 0;i < k;++i) {
-            ump[co[i]] = i;
-        }
-        for (ll i = 0;i < k;++i) {
-            ans[i][ump[ro[i]]] = ro[i] + 1;
+        ans.assign(k, vi(int)(k, 0));
+        for(ll i=0;i<k;++i){
+            ll num=r[i], pos=-1;
+            for(ll j=0;j<k;++j){
+                if(c[j]==num){
+                    pos=j;
+                    break;
+                }
+            }
+            ans[i][pos]=num;
         }
         return ans;
     }
