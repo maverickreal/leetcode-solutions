0826b8d031914c9b58a5b59ecb66fc72ae52c2ba
Aditya Saxena <adity09@outlook.com>
2022-10-22 14:13:25 +0530

    Time: 664 ms (18.18%), Space: 67.2 MB (9.09%) - LeetHub

--- 2247-maximum-cost-of-trip-with-k-highways/2247-maximum-cost-of-trip-with-k-highways.cpp
+++ 2247-maximum-cost-of-trip-with-k-highways/2247-maximum-cost-of-trip-with-k-highways.cpp
@@ -1,4 +1,3 @@
-
 class Solution {
 typedef long long ll;
 typedef pair<ll, ll> pi;
@@ -9,19 +8,20 @@ const char nl = '\n';
 public:
     vi(vi(pi))g;
     vi(bool)vis;
-    vi(vi(vi(ll)))dp;
-    ll func(ll nd, ll k, ll bm){
+    vi(vi(ll))dp;
+    ll k;
+    ll func(ll nd, ll bm){
         if(vis[nd]){
             return -1;
         }
-        if(k==1){
+        if(__builtin_popcount(bm)==k){
             return 0;
         }
-        ll&ans=dp[nd][k][bm];
+        ll&ans=dp[nd][bm];
         if(ans==LLONG_MAX){
             vis[nd]=1, ans=-1;
             for(const pi&p:g[nd]){
-                ll rec=func(p.first, k-1, bm|(1<<nd));
+                ll rec=func(p.first, bm|(1<<nd));
                 if(rec>-1){
                     rec+=p.second;
                 }
@@ -31,14 +31,14 @@ public:
         }
         return ans;
     }
-    int maximumCost(int n, vector<vector<int>>&e, int k) {
-        g.resize(n), vis.resize(n), dp.assign(n, vi(vi(ll))(k+2, vi(ll)(1<<n, LLONG_MAX)));
+    int maximumCost(int n, vector<vector<int>>&e, int K) {
+        k=K, g.resize(n), vis.resize(n), dp.assign(n, vi(ll)(1<<n, LLONG_MAX));
         for(const auto&edge:e){
             g[edge[0]].pb({edge[1], edge[2]}), g[edge[1]].pb({edge[0], edge[2]});
         }
         ll ans=-1;
         for(ll nd=0;nd<n;++nd){
-            ans=max(ans, func(nd, k+1, 0));
+            ans=max(ans, func(nd, 0));
         }
         return ans;
     }
