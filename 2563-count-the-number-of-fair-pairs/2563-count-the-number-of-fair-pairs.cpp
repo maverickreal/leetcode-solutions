d37edfee3a131226ece234b0ef67c12013e9e84e
Aditya Saxena <adity09@outlook.com>
2023-02-12 09:33:17 +0530

    Time: 208 ms (50.00%), Space: 53.5 MB (16.67%) - LeetHub

---
+++ 2563-count-the-number-of-fair-pairs/2563-count-the-number-of-fair-pairs.cpp
@@ -0,0 +1,55 @@
+class Solution {
+public:
+typedef long long ll;
+#define vi(x) vector<x>
+typedef pair<ll, ll> pi;
+#define pb push_back
+const ll mod = 1e9+7;
+const char nl = '\n';
+
+ll bsLow(const vi(int)&v, ll it, ll l, ll r){
+    ll ans=-1;
+    while(l<=r){
+        ll m=l+((r-l)>>1);
+        if(v[m]<it){
+            l=m+1;
+        }
+        else{
+            ans=m, r=m-1;
+        }
+    }
+    return ans;
+}
+
+ll bsHigh(const vi(int)&v, ll it, ll l, ll r){
+    ll ans=-1;
+    while(l<=r){
+        ll m=l+((r-l)>>1);
+        if(v[m]>it){
+            r=m-1;
+        }
+        else{
+            ans=m, l=m+1;
+        }
+    }
+    return ans;
+}
+
+long long countFairPairs(vector<int>&v, int low, int high) {
+    sort(begin(v), end(v));
+    // for(ll it:v){
+    //     cout<<it<<' ';
+    // }
+    // cout<<nl;
+    ll ans=0, n=v.size();
+    for(ll i=0;i<n;++i){
+        ll l=low-v[i], r=high-v[i];
+        ll lIn=bsLow(v, l, 0, i-1), rIn=bsHigh(v, r, 0, i-1);
+        if(!(lIn==-1  || rIn==-1 || lIn>rIn)){
+            ans+=rIn-lIn+1;
+        }
+        //cout<<i<<' '<<l<<' '<<r<<' '<<lIn<<' '<<rIn<<' '<<ans<<nl;
+    }
+    return ans;
+}
+};
