de363a31cadfb83b18accd1a0c102cc83a118a17
Aditya Saxena <adity09@outlook.com>
2023-02-26 10:16:06 +0530

    Time: 1428 ms (33.33%), Space: 131.2 MB (50.00%) - LeetHub

--- 2577-minimum-time-to-visit-a-cell-in-a-grid/2577-minimum-time-to-visit-a-cell-in-a-grid.cpp
+++ 2577-minimum-time-to-visit-a-cell-in-a-grid/2577-minimum-time-to-visit-a-cell-in-a-grid.cpp
@@ -10,27 +10,15 @@ const char nl = '\n';
 class cmp{
     public:
     bool operator()(const vi(ll)&a, const vi(ll)&b){
-        if(a[2]!=b[2]){
-            return (a[2]>b[2]);
-        }
-        if(a[1]!=b[1]){
-            return (a[1]<b[1]);
-        }
-        return (a[0]<b[0]);
+        return (a[2]>b[2]);
     }
 };
-    
-ll m, n;
-
-bool exists(const ll&i, const ll&j){
-    return (i>-1 && j>-1 && i<m && j<n);
-}
 
 int minimumTime(vector<vector<int>>&v) {
     if(v[0][1]>1 && v[1][0]>1){
         return -1;
     }
-    m=v.size(), n=v[0].size();
+    ll m=v.size(), n=v[0].size();
     vi(vi(ll))dirs={{0, -1}, {-1, 0}, {0, 1}, {1, 0}};
     vi(vi(bool))vis(m, vi(bool)(n, 0));
     priority_queue<vi(ll), vi(vi(ll)), cmp>pq;
@@ -42,13 +30,9 @@ int minimumTime(vector<vector<int>>&v) {
         if(it[0]==m-1 && it[1]==n-1){
             return it[2];
         }
-        // if(!exists(it[0], it[1]) || vis[it[0]][it[1]]){
-        //     continue;
-        // }
-        // vis[it[0]][it[1]]=1;
         for(const vi(ll)&dir:dirs){
             ll x=it[0]+dir[0], y=it[1]+dir[1];
-            if(!exists(x, y) || vis[x][y]){
+            if(x<0 || y<0 || x>=m || y>=n || vis[x][y]){
                 continue;
             }
             vis[x][y]=1;
