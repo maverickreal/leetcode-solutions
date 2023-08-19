3ed00cf8deaa2b8a8b0a886e46438ce927330071
Aditya Saxena <adity09@outlook.com>
2023-08-19 14:52:10 +0530

    Time: 0 ms (100.00%), Space: 6.2 MB (78.50%) - LeetHub

--- 0020-valid-parentheses/0020-valid-parentheses.cpp
+++ 0020-valid-parentheses/0020-valid-parentheses.cpp
@@ -1,53 +1,27 @@
 class Solution {
 public:
-    char getClosing(char ch){
-        if(ch=='('){
-            return ')';
+    char getOpen(char ch){
+        if(ch==')'){
+            return '(';
         }
-        if(ch=='{'){
-            return '}';
+        if(ch==']'){
+            return '[';
         }
-        return ']';
-    }
-    bool isOpening(char ch){
-        return (ch=='(' || ch=='{' || ch=='[');
-    }
-    int find(const string&str, int start, char toFind){
-        int pa=0, cu=0, br=0;
-        for(int i=start; i<str.size(); ++i){
-            if(str[i]==toFind && pa==0 && cu==0 && br==0){
-                return i;
-            }
-            if(str[i]=='('){
-                ++pa;
-            } else if(str[i]=='{'){
-                ++cu;
-            } else if(str[i]=='['){
-                ++br;
-            } else if(str[i]==')'){
-                --pa;
-            } else if(str[i]=='}'){
-                --cu;
-            } else if(str[i]==']'){
-                --br;
-            }
-        }
-        return -1;
+        return '{';
     }
     bool isValid(string s) {
-        for(int i=0; i<s.size(); ++i){
-            if(s[i]=='#'){
+        int sz = s.size();
+        stack<char>st;
+        for(int i=0; i<sz; ++i){
+            if(s[i]=='(' || s[i]=='{' || s[i]=='['){
+                st.push(s[i]);
                 continue;
             }
-            if(!isOpening(s[i])){
+            if(st.empty() || st.top()!=getOpen(s[i])){
                 return false;
             }
-            int found = find(s, i+1, getClosing(s[i]));
-            if(found==-1){
-                return false;
-            }
-            s[found]='#';
+            st.pop();
         }
-        return true;
+        return st.empty();
     }
 };
