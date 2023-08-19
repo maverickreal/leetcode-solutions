ac9325e4aacfb911f78709be1a03163fd950e8b2
Aditya Saxena <adity09@outlook.com>
2023-08-19 16:14:52 +0530

    Time: 0 ms (100.00%), Space: 6.3 MB (37.65%) - LeetHub

--- 0020-valid-parentheses/0020-valid-parentheses.cpp
+++ 0020-valid-parentheses/0020-valid-parentheses.cpp
@@ -1,55 +1,27 @@
 class Solution {
 public:
-    int sz;
-    bool isOpen(char ch){
-        return (ch=='(' || ch=='{' || ch=='[');
-    }
-    char getClosing(char ch){
-        if(ch=='('){
-            return ')';
+    char getOpen(char ch){
+        if(ch==')'){
+            return '(';
         }
-        if(ch=='{'){
-            return '}';
+        if(ch==']'){
+            return '[';
         }
-        return ']';
-    }
-    int getClosingPos(const string&str, int start, char toFind){
-        int pa=0, cu=0, br=0;
-        for(int i=start; i<sz; ++i){
-            if(str[i]==toFind && pa==0 && cu==0 && br==0){
-                return i;
-            }
-            if(str[i]=='('){
-                ++pa;
-            } else if(str[i]=='{'){
-                ++cu;
-            } else if(str[i]=='['){
-                ++br;
-            } else if(str[i]==')'){
-                --pa;
-            } else if(str[i]=='}'){
-                --cu;
-            } else if(str[i]==']'){
-                --br;
-            }
-        }
-        return -1;
+        return '{';
     }
     bool isValid(string s) {
-        sz = s.size();
-        for(int i=0; i<sz; ++i){
-           if(s[i]=='#'){
-               continue;
-           }
-            if(!isOpen(s[i])){
-                return false;
+        stack<char>st;
+        for(int i=0; i<s.size(); ++i){
+            if(s[i]=='(' || s[i]=='[' || s[i]=='{'){
+                st.push(s[i]);
+            } else{
+               if(!st.empty() && st.top()==getOpen(s[i])){
+                   st.pop();
+               }else{
+                   return false;
+               }
             }
-            int found = getClosingPos(s, i+1, getClosing(s[i]));
-            if(found==-1){
-                return false;
-            }
-            s[found]='#';
         }
-        return true;
+        return st.empty();
     }
 };
