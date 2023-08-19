5430f97699c455ce8baaa9a8f0378424310f6760
Aditya Saxena <adity09@outlook.com>
2023-08-19 15:57:29 +0530

    Time: 30 ms (25.88%), Space: 6.3 MB (37.65%) - LeetHub

--- 0020-valid-parentheses/0020-valid-parentheses.cpp
+++ 0020-valid-parentheses/0020-valid-parentheses.cpp
@@ -1,27 +1,55 @@
 class Solution {
 public:
-    char getOpen(char ch){
-        if(ch==')'){
-            return '(';
+    int sz;
+    bool isOpen(char ch){
+        return (ch=='(' || ch=='{' || ch=='[');
+    }
+    char getClosing(char ch){
+        if(ch=='('){
+            return ')';
         }
-        if(ch==']'){
-            return '[';
+        if(ch=='{'){
+            return '}';
         }
-        return '{';
+        return ']';
+    }
+    int getClosingPos(const string&str, int start, char toFind){
+        int pa=0, cu=0, br=0;
+        for(int i=start; i<sz; ++i){
+            if(str[i]==toFind && pa==0 && cu==0 && br==0){
+                return i;
+            }
+            if(str[i]=='('){
+                ++pa;
+            } else if(str[i]=='{'){
+                ++cu;
+            } else if(str[i]=='['){
+                ++br;
+            } else if(str[i]==')'){
+                --pa;
+            } else if(str[i]=='}'){
+                --cu;
+            } else if(str[i]==']'){
+                --br;
+            }
+        }
+        return -1;
     }
     bool isValid(string s) {
-        int sz = s.size();
-        stack<char>st;
+        sz = s.size();
         for(int i=0; i<sz; ++i){
-            if(s[i]=='(' || s[i]=='{' || s[i]=='['){
-                st.push(s[i]);
-                continue;
-            }
-            if(st.empty() || st.top()!=getOpen(s[i])){
+           if(s[i]=='#'){
+               continue;
+           }
+            if(!isOpen(s[i])){
                 return false;
             }
-            st.pop();
+            int found = getClosingPos(s, i+1, getClosing(s[i]));
+            if(found==-1){
+                return false;
+            }
+            s[found]='#';
         }
-        return st.empty();
+        return true;
     }
 };
