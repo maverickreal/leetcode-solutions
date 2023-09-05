c98303fa401ee1749d237dd78ca099b22932aae4
Aditya Saxena <adity09@outlook.com>
2023-09-05 17:39:38 +0530

    Time: 0 ms (100%), Space: 6.1 MB (41.05%) - LeetHub

--- 0886-score-of-parentheses/0886-score-of-parentheses.cpp
+++ 0886-score-of-parentheses/0886-score-of-parentheses.cpp
@@ -1,21 +1,29 @@
 class Solution {
 public:
-    int helper(const string&str, int l, int r){
-        if(l>=r){
-            return 0;
-        }
-        int ans=0, bal=0;
-        for(int i=l, j=l; i<=r; ++i){
-            bal += str[i]=='(' ? 1 : -1;
-            if(bal==0){
-                int rec = helper(str, j+1, i-1);
-                ans += rec ? 2*rec : 1;
-                j=i+1;
+    int scoreOfParentheses(string s) {
+        stack<int>st;
+        for(int i=0; i<s.size(); ++i){
+            if(s[i]=='('){
+                st.push(-1);
+            } else{
+                int res=0;
+                while(st.top()!=-1){
+                    res+=st.top();
+                    st.pop();
+                }
+                st.pop();
+                if(res==0){
+                    st.push(1);
+                } else{
+                    st.push(2*res);
+                }
             }
         }
+        int ans = 0;
+        while(!st.empty()){
+            ans+=st.top();
+            st.pop();
+        }
         return ans;
-    }
-    int scoreOfParentheses(string s) {
-      return helper(s, 0, s.size()-1);  
     }
 };
