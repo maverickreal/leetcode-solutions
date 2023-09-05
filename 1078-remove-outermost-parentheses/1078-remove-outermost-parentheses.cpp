c65956499b7dd0080920c8f129a10f0456c9c1b1
Aditya Saxena <adity09@outlook.com>
2023-09-05 14:48:19 +0530

    Time: 35 ms (5.3%), Space: 6.7 MB (47.45%) - LeetHub

---
+++ 1078-remove-outermost-parentheses/1078-remove-outermost-parentheses.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    string removeOuterParentheses(string s) {
+        int bal=0, pre=0;
+        for(int i=0 ;i<s.size(); ++i){
+            if(s[i]=='('){
+                ++bal;
+            } else{
+                --bal;
+            }
+            if(!bal){
+                s.erase(begin(s)+pre);
+                --i;
+                s.erase(begin(s)+i);
+                --i;
+                pre=i+1;
+            }
+        }
+        return s;
+    }
+};
