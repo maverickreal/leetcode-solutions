ede1cae9f155a61c4955e8c75690f80ee1b15215
Aditya Saxena <adity09@outlook.com>
2023-09-28 02:15:25 +0530

    Time: 26 ms (93.71%), Space: 9.7 MB (95.36%) - LeetHub

--- 1542-find-longest-awesome-substring/1542-find-longest-awesome-substring.cpp
+++ 1542-find-longest-awesome-substring/1542-find-longest-awesome-substring.cpp
@@ -1,22 +1,17 @@
 class Solution {
-typedef long long ll;
-typedef pair<ll, ll> pi;
-#define vi(x) vector<x>
-#define pb push_back
-const ll mod = 1e9 + 7;
-const char nl = '\n';
 public:
-    int longestAwesome(const string&s) {
-        ll n=s.size(), bm=0, ans=1;
-        vi(ll)ump(1024, n);
+    int longestAwesome(string&str) {
+        int n=str.size(), bm=0, ans=1;
+        vector<int>ump(1<<10, n);
         ump[0]=-1;
-        for(ll i=0;i<n;++i){
-            bm^=1<<(s[i]-'0');
-            ans=max(ans, i-ump[bm]);
-            for(ll j=0;j<10;++j){
-                ans=max(ans, i-ump[bm^(1<<j)]);
+        for(int i=0; i<n; ++i){
+            bm^=1<<(str[i]-'0');
+            int res = i-ump[bm];
+            for(int j=0; j<10; ++j){
+                res = max(res, i-ump[bm^(1<<j)]);
             }
-            ump[bm]=min(ump[bm], i);
+            ans=max(ans, res);
+            ump[bm] = min(ump[bm], i);
         }
         return ans;
     }
