059b1bfa2d46ef146f01251a7f7691b05479c897
Aditya Saxena <adity09@outlook.com>
2023-09-29 18:10:12 +0530

    Time: 84 ms (34.37%), Space: 38.7 MB (37.47%) - LeetHub

--- 2556-disconnect-path-in-a-binary-matrix-by-at-most-one-flip/2556-disconnect-path-in-a-binary-matrix-by-at-most-one-flip.cpp
+++ 2556-disconnect-path-in-a-binary-matrix-by-at-most-one-flip/2556-disconnect-path-in-a-binary-matrix-by-at-most-one-flip.cpp
@@ -1,32 +1,22 @@
 class Solution {
 public:
-typedef long long ll;
-#define vi(x) vector<x>
-typedef pair<ll, ll> pi;
-#define pb push_back
-const ll mod = 1e9+7;
-const char nl = '\n';
-    
-ll m,n;
-
-bool dfs(vi(vi(int))&g, ll i, ll j){
-    if(i<0 || j<0 || i>=m || j>=n || !g[i][j]){
-        return 0;
+    int m, n;
+    int dfs(vector<vector<int>>&v, int i, int j){
+        if(i<0 || i>=m || j<0 || j>=n || !v[i][j]){
+            return 0;
+        }
+        v[i][j]=0;
+        if(i==m-1 && j==n-1){
+            return 1;
+        }
+        return dfs(v, i, j+1) || dfs(v, i+1, j);
     }
-    g[i][j]=0;
-    if(i==m-1 && j==n-1){
-        return 1;
+    bool isPossibleToCutPath(vector<vector<int>>&v) {
+        m=v.size(), n=v[0].size();
+        if(!dfs(v, 0, 0)){
+            return 1;
+        }
+        v[0][0]=v[m-1][n-1]=1;
+        return !dfs(v, 0, 0);
     }
-    return (dfs(g, i, j+1) || dfs(g, i+1, j));
-}
-
-bool isPossibleToCutPath(vector<vector<int>>&g) {
-    m=g.size(), n=g[0].size();
-    bool res = dfs(g, 0, 0);
-    if(!res){
-        return 1;
-    }
-    g[0][0]=g[m-1][n-1]=1;
-    return !dfs(g, 0, 0);
-}
 };
