24b63ba0fdcd89ccb3db5ed9b8f48b57dd921c05
Aditya Saxena <adity09@outlook.com>
2023-09-29 19:13:32 +0530

    Time: 344 ms (82.42%), Space: 141.2 MB (64.84%) - LeetHub

--- 1687-delivering-boxes-from-storage-to-ports/1687-delivering-boxes-from-storage-to-ports.cpp
+++ 1687-delivering-boxes-from-storage-to-ports/1687-delivering-boxes-from-storage-to-ports.cpp
@@ -1,21 +1,16 @@
 class Solution {
 public:
-    int boxDelivering(vector<vector<int>>& boxes, int pc, int mb, int mw) {
-        int n = boxes.size();
+    int boxDelivering(vector<vector<int>>& boxes, int ports, int mxb, int mxw) {
+        int n=boxes.size();
         vector<int>dp(n, INT_MAX);
-        int cw=0, cb=0;
-        for(int i=0, j=0; i<n; ++i){
-            cw += boxes[i][1];
-            cb += !(j<i && boxes[i-1][0]==boxes[i][0]);
-            while(j<i && (cw>mw || i-j+1>mb || (j && dp[j]<=dp[j-1]))){
-                cw-=boxes[j][1];
-                cb-=boxes[j][0]!=boxes[j+1][0];
-                ++j;
+        int i=0, j=0, cw=0, cb=0;
+        for(; j<n; ++j){
+            cw+=boxes[j][1]; cb+=j==i || boxes[j][0]!=boxes[j-1][0];
+            while(i<j && (cw>mxw || j-i+1>mxb || (i && dp[i]<=dp[i-1]))){
+               cw-=boxes[i][1]; cb-=boxes[i][0]!=boxes[i+1][0]; ++i;
             }
-            dp[i] = (j ? dp[j-1] : 0) + cb + 1;
-            // cout<<cb<<' '<<cw<<' '<<dp[i]<<endl;
+            dp[j]=(i?dp[i-1]:0) + cb + 1;
         }
-
-        return dp.back();
+        return dp[n-1];
     }
 };
