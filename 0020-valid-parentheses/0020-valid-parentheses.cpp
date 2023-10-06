78e5ce03d760a07093d2af649afb32fe85daa579
Aditya Saxena <adity09@outlook.com>
2023-10-06 15:06:36 +0530

    Time: 0 ms (100.00%), Space: 6.6 MB (19.93%) - LeetHub

--- 0020-valid-parentheses/0020-valid-parentheses.cpp
+++ 0020-valid-parentheses/0020-valid-parentheses.cpp
@@ -1,25 +1,20 @@
 class Solution {
 public:
-    char getOpen(char ch){
-        if(ch==')'){
-            return '(';
-        }
-        if(ch==']'){
-            return '[';
-        }
-        return '{';
-    }
     bool isValid(string s) {
         stack<char>st;
-        for(int i=0; i<s.size(); ++i){
-            if(s[i]=='(' || s[i]=='[' || s[i]=='{'){
-                st.push(s[i]);
+        for(char ch : s){
+            if(ch=='(' || ch=='{' || ch=='['){
+                st.push(ch);
+            } else if(st.empty()){
+                return false;
+            } else if(ch==')' && st.top()!='('){
+                return false;
+            } else if(ch==']' && st.top()!='['){
+                return false;
+            } else if(ch=='}' && st.top()!='{'){
+                return false;
             } else{
-               if(!st.empty() && st.top()==getOpen(s[i])){
-                   st.pop();
-               }else{
-                   return false;
-               }
+                st.pop();
             }
         }
         return st.empty();
