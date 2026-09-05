75b237afed8b5793e8ace179a1aa2f6d52386570
Aditya Saxena <adity09@outlook.com>
2026-09-05 19:33:13 +0530

    Time: 7 ms (90%), Space: 79.6 MB (50%) - LeetHub

---
+++ 3904-smallest-stable-index-ii/3904-smallest-stable-index-ii.ts
@@ -0,0 +1,19 @@
+function firstStableIndex(nums: number[], k: number): number {
+    const sz = nums.length;
+    const mx = Uint32Array.from(nums);
+    const mn = Uint32Array.from(nums);
+
+    for (let i: number = 0; i < sz; ++i) {
+        mx[i] = (i === 0 || mx[i] > mx[i - 1]) ? mx[i] : mx[i - 1];
+        mn[sz - i - 1] = (i === 0 || mn[sz - i - 1] < mn[sz - i]) ?
+            mn[sz - i - 1] : mn[sz - i];
+    }
+
+    for (let i: number = 0; i < sz; ++i) {
+        if ((mx[i] - mn[i]) <= k) {
+            return i;
+        }
+    }
+
+    return -1;
+};
