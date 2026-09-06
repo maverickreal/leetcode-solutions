6ce148e05cbb1e30c37500b21e117392ec99c5cd
Aditya Saxena <adity09@outlook.com>
2026-09-06 21:05:30 +0530

    Time: 23 ms (68%), Space: 55.7 MB (100%) - LeetHub

--- 0115-distinct-subsequences/0115-distinct-subsequences.ts
+++ 0115-distinct-subsequences/0115-distinct-subsequences.ts
@@ -1,27 +1,29 @@
-let dp: Array<Uint32Array>;
+let dp: Uint32Array;
 let szs: number;
 let szt: number;
 
-const func = (str: string, tar: string, i: number, j: number): number => {
-    dp[szs][szt] = 1;
+const func = (str: string, tar: string): number => {
+    dp[szt] = 1;
+    let res: Uint32Array = new Uint32Array(szt + 1);
 
     for (let i: number = szs - 1; i > -1; --i) {
         for (let j: number = i; j > -1; --j) {
-            dp[i][j] = dp[i + 1]?.[j] ?? 0;
+            res[j] = dp[j] ?? 0;
 
             if (str[i] === tar[j]) {
-                dp[i][j] += dp[i + 1]?.[j + 1] ?? 0;
+                res[j] += dp[j + 1] ?? 0;
             }
         }
+        [dp, res] = [res, dp];
     }
 
-    return dp[0][0];
+    return dp[0];
 };
 
 function numDistinct(s: string, t: string): number {
     szt = t.length;
     szs = s.length;
-    dp = Array.from({ length: szs + 1 }, () => new Uint32Array(szt + 1).fill(0));
+    dp = new Uint32Array(szt + 1);
 
-    return func(s, t, 0, 0);
+    return func(s, t);
 };
