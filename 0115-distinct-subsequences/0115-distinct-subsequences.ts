6734474600ccde1322d341411f3d1604b1594c4d
Aditya Saxena <adity09@outlook.com>
2026-09-06 20:57:19 +0530

    Time: 31 ms (62%), Space: 72.1 MB (52%) - LeetHub

--- 0115-distinct-subsequences/0115-distinct-subsequences.ts
+++ 0115-distinct-subsequences/0115-distinct-subsequences.ts
@@ -1,33 +1,27 @@
-let dp: Array<Int32Array>;
+let dp: Array<Uint32Array>;
 let szs: number;
 let szt: number;
 
 const func = (str: string, tar: string, i: number, j: number): number => {
-    if (j === szt) {
-        return 1;
+    dp[szs][szt] = 1;
+
+    for (let i: number = szs - 1; i > -1; --i) {
+        for (let j: number = i; j > -1; --j) {
+            dp[i][j] = dp[i + 1]?.[j] ?? 0;
+
+            if (str[i] === tar[j]) {
+                dp[i][j] += dp[i + 1]?.[j + 1] ?? 0;
+            }
+        }
     }
 
-    if (i === szs) {
-        return 0;
-    }
-
-    if (dp[i][j] !== -1) {
-        return dp[i][j];
-    }
-    const dont = func(str, tar, i + 1, j);
-    let take: number = 0;
-
-    if (str[i] === tar[j]) {
-        take = func(str, tar, i + 1, j + 1);
-    }
-
-    return dp[i][j] = dont + take;
+    return dp[0][0];
 };
 
 function numDistinct(s: string, t: string): number {
     szt = t.length;
     szs = s.length;
-    dp = Array.from({ length: szs }, () => new Int32Array(szt).fill(-1));
+    dp = Array.from({ length: szs + 1 }, () => new Uint32Array(szt + 1).fill(0));
 
     return func(s, t, 0, 0);
 };
